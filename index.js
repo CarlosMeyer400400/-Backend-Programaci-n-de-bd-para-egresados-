c75dd5722541d8681329be919ea816af79b0f9f7
CARLOSMEYER <20210696@uthh.edu.mx>
2025-01-17 14:18:24 -0600

    segunda parte backend parte 2 y 3 del formulario

--- index.js
+++ index.js
@@ -76,7 +76,41 @@ app.post('/usuarios/agregar', (req, res) => {
         correo_institucional: req.body.correo_institucional,
         correo_personal: req.body.correo_personal,
         estatus_laboral: req.body.estatus_laboral,
-        estudiando: req.body.estudiando 
+        estudiando: req.body.estudiando, 
+
+//NO TRABAJA 
+       conoce_bolsa_trabajo: req.body.conoce_bolsa_trabajo,
+       utiliza_bolsa_trabajo: req.body.utiliza_bolsa_trabajo,
+       calidad_servicio: req.body.calidad_servicio,
+       capacitacion_postegreso: req.body.capacitacion_postegreso,
+       sugerencias: req.body.sugerencias,
+
+//SI TRABAJA        
+
+       medio_colocacion: req.body.medio_colocacion,
+       localidad_empleo: req.body.localidad_empleo,
+       domicilio_empresa: req.body.domicilio_empresa,
+       tiempo_colocacion: req.body.tiempo_colocacion,
+       tipo_organizacion: req.body.tipo_organizacion,
+       tamano_organizacion: req.body.tamaño_organizacion,
+       nombre_empresa: req.body.nombre_empresa,
+       giro_empresa: req.body.giro_empresa,
+       sueldo_mensual: req.body.sueldo_mensual,
+       tiene_prestaciones: req.body.tiene_prestaciones,
+       ocupacion: req.body.ocupacion,
+       puesto_actual: req.body.puesto_actual,
+       trabaja_perfil_egreso: req.body.trabaja_perfil_egreso,
+       nombre_jefe: req.body.nombre_jefe,
+       telefono_jefe: req.body.telefono_jefe,
+       numero_trabajos: req.body.numero_trabajos,
+       puestos_directivos: req.body.puestos_directivos,
+       empresa_propia: req.body.empresa_propia,
+       experiencia_transnacional: req.body.experiencia_transnacional,
+       solucion_problemas: req.body.solucion_problemas,
+       desempeno_funciones: req.body.desempeño_funciones,
+       comunicacion_ideas: req.body.comunicacion_ideas,
+       capacidad_liderazgo: req.body.capacidad_liderazgo,
+       habilidades_faltantes: req.body.habilidades_faltantes,
     };
 
     const query = `INSERT INTO usuarios SET ?`
